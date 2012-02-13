4d4263beb577065fc4f30408c5b8324765c49085
Sergio <speleato@gmail.com>
2012-02-13 23:46:46 +0100

    Update RandomSearch.rb

--- RandomSearch.rb
+++ RandomSearch.rb
@@ -21,7 +21,17 @@ $activity.start_ruboto_activity "$sample_activity" do
  
  
  @ManualBusqueda = proc do |view|
-      @text_view.text = 'Prueba a ver si sale el texto'
+      @text_view.text = 'La busqueda aleatoria plantea un metodo base para la búsqueda de elementos en un espacio acotado. El algoritmo sirve de inspiración para la búsqueda aleatoria directa y adaptativa.
+						 La estrategia seguida consiste en tomar soluciones aleatoriamente en el espacio de búsqueda usando una funcion de distribución probabilistica. Una vez con un conjunto de muestras, el siguiente
+						 conjunto se tomará basandose en esa función de probabilidad. Este algoritmo ofrece resultados rápidos, pero tiende a atascarse en óptimos locales. Los parámetros de configuración claves serán 
+						 la función de construcción de candidatos y la función de evaluación de soluciones, las cuales determinarán la velocidad y la precisión de este algoritmo.
+						 Parametros del algoritmo ejemplo: 
+						 --> tamaño del problema: 2
+						 --> espacio de búsqueda: [-5, 5]
+						 -->iteraciones máximas: 100
+						 Recomendaciones:
+						 1/ Ofrece soluciones aproximadas a la solución óptima en tiempos limitados siempre que se ejecute en entornos acotados pequeños. En entornos grandes su rendimiento tiende a descender
+						 2/ En ocasiones puede ser usado para hallar un punto de partida mejor para algoritmos mas adecuados a las características de un problema ofreciendo una solucion inicial mejor.'
   end
   
   
@@ -53,19 +63,9 @@ def search(search_space, max_iter)
 	  candidate[:cost] = objective_function(candidate[:vector])
 	  best = candidate if best.nil? or candidate[:cost] < best[:cost]
 	  puts " > iteration=#{(iter+1)}, best=#{best[:cost]}"
+	  iteration = iter if best.nil? or candidate[:cost] < best[:cost] 
 	end
   return best
 end
- 
-#if __FILE__ == $0
-  # problem configuration
- # problem_size = 2
-  #search_space = Array.new(problem_size) {|i| [-5, +5]}
-  #Chapter 2. Stochastic Algorithms
-  # algorithm configuration
-  # max_iter = 100
-  # execute the algorithm
-  #best = search(search_space, max_iter)
-  #puts "Done. Best Solution: c=#{best[:cost]}, v=#{best[:vector].inspect}"
-  #end
+
 end

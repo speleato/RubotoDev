eaee69adc173e69924332d1ae0081e4c23193552
Sergio <speleato@gmail.com>
2012-02-13 23:48:45 +0100

    Update StochasticHillClimbing.rb

--- StochasticHillClimbing.rb
+++ StochasticHillClimbing.rb
@@ -5,7 +5,7 @@ require 'ruboto/util/toast'
 ruboto_import_widgets :Button, :LinearLayout, :TextView
 
 $activity.start_ruboto_activity "$sample_activity" do
-  setTitle 'Adaptative Random Search'
+  setTitle 'Stochastic hill climbing'
 
   
   
@@ -23,7 +23,16 @@ $activity.start_ruboto_activity "$sample_activity" do
   
   
 @ManualBusqueda = proc do |view|
-      @text_view.text = 'Prueba a ver si sale el texto'
+      @text_view.text = 'El algoritmo consiste en buscar un elemento en un espacio de búsqueda mediante la valoración de vecinos aleatorios al individuo actual. Ofrece la ventaja
+	  de ser capaz de sobreponerse a algunos óptimos locales, lo cual supone una mejora a los algoritmos hill-climbing estandar. 
+	  Parametros del algoritmo ejemplo:
+	  -->numero de bits: 64
+	  -->maximo numero de iteraciones: 1000
+	  Recomendaciones
+	  1/ Este algoritmo está diseñado para funcionar en dominios discretos.
+	  2/ El proceso de busqueda está diseñado para busquedas locales y se recomienda su uso para optimizar soluciones ya previamente filtradas por un algoritmo de búsqueda más global
+	  3/ A pesar de la mejora, en algunos casos puede quedarse atascado en óptimos locales.
+	  4/ Este procedimiento puede ser aplicado a varias soluciones candidatas al unísono'
 end
    
 @Ejecutar = proc do |view|
